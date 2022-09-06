3033de6d4ee12dd44c8e1b02e6c53b2e5174519b
Caleb1418 <Caleboliver1418@gmail.com>
2022-09-06 13:54:45 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/result.dart';
+import './quiz.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite colour?',
+      'answers': [
+        {'text': 'black', 'score': 10},
+        {'text': 'red', 'score': 6},
+        {'text': 'white', 'score': 4},
+        {'text': 'purple', 'score': 8}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'dog', 'score': 8},
+        {'text': 'cat', 'score': 9},
+        {'text': 'rabbit', 'score': 4},
+        {'text': 'bird', 'score': 2}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite car?',
+      'answers': [
+        {'text': 'bmw', 'score': 10},
+        {'text': 'ford', 'score': 4},
+        {'text': 'mercedes', 'score': 8},
+        {'text': 'jeep', 'score': 7}
+      ]
+    },
+  ];
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    if (_questionIndex < _questions.length) {
+      print('We have more questions');
+    }
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Quiz app'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result()),
+    );
+  }
+}
